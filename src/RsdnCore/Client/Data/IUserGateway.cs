6da52d06e6b0901c478c53ac08b6ae33f8a9b974
Vladislav Borovikov <vborovikov@live.ru>
2017-07-06 23:07:29 +0500

    added activity stubs

---
+++ src/RsdnCore/Client/Data/IUserGateway.cs
@@ -0,0 +1,14 @@
+﻿namespace Rsdn.Client.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Community.Interaction;
+
+    public interface IUserGateway : IGateway
+    {
+        IEnumerable<ThreadModel> GetActivity(int userId);
+    }
+}
