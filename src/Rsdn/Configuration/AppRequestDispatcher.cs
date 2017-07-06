0410182a8d37a833ea753858b3936c3a95ad97c1
Vladislav Borovikov <vborovikov@live.ru>
2017-07-07 03:38:08 +0500

    added app request dispatcher

---
+++ src/Rsdn/Configuration/AppRequestDispatcher.cs
@@ -0,0 +1,17 @@
+﻿namespace Rsdn.Configuration
+{
+    using System;
+    using Relay.RequestModel.Default;
+
+    internal class AppRequestDispatcher : DefaultRequestDispatcher
+    {
+        private const string App = "App";
+
+        public AppRequestDispatcher(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+        }
+
+        public override string ToString() => App;
+    }
+}
