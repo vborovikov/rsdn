b2fb97653d888baf4cce7bd65744be9ad23811da
Vladislav Borovikov <vborovikov@live.ru>
2017-07-07 03:27:33 +0500

    used UWP Community Toolkit controls

---
+++ src/RsdnFoundation/Xaml/Controls/PullToRefreshMasterDetailsView.cs
@@ -0,0 +1,24 @@
+﻿namespace Rsdn.Xaml.Controls
+{
+    using System.Windows.Input;
+    using Microsoft.Toolkit.Uwp.UI.Controls;
+    using Windows.UI.Xaml;
+
+    public sealed class PullToRefreshMasterDetailsView : MasterDetailsView
+    {
+        public static readonly DependencyProperty RefreshCommandProperty =
+            DependencyProperty.Register(nameof(RefreshCommand), typeof(ICommand),
+                typeof(PullToRefreshMasterDetailsView), new PropertyMetadata(null));
+
+        public PullToRefreshMasterDetailsView()
+        {
+            this.DefaultStyleKey = typeof(PullToRefreshMasterDetailsView);
+        }
+
+        public ICommand RefreshCommand
+        {
+            get { return (ICommand)GetValue(RefreshCommandProperty); }
+            set { SetValue(RefreshCommandProperty, value); }
+        }
+    }
+}
